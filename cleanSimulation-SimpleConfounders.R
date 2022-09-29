fc84d70c56216fecc5d8338c6d98726967cc264a
S.A.Malec, PhD <kingfish777@users.noreply.github.com>
2022-09-29 12:42:49 -0400

    updated clean simulation plus extras

--- cleanSimulation-SimpleConfounders.R
+++ cleanSimulation-SimpleConfounders.R
@@ -12,6 +12,8 @@ library(ggplot2)
 library(earth) 
 library(ranger)
 library(dagitty)
+library(gt)
+library(simcausal)
 
 set.seed(7) # so results are reproducible
 
@@ -123,7 +125,177 @@ exp(confint(naive))
 # exponentiated odds ratio = 2.608
 # True mOR = 2.52 (!!!)
 
+## TMLE implementation by hand
+# Step 1 estimation and prediction of the model for the outcome (G-computation)
+gm <- glm(data = ObsData, Y ~ A + W1 + W2 + W3 + W4, family = binomial)
+# Prediction for A, A = 1 and, A = 0
+QAW_0 <- predict(gm, type = "response")
+Q1W_0 = predict(gm, newdata=data.frame(A = 1, ObsData [,c("W1","W2","W3","W4")]), type = "response")
+Q0W_0 = predict(gm, newdata=data.frame(A = 0, ObsData [,c("W1","W2","W3","W4")]), type = "response")
+# Estimated mortality risk difference
+mean(Q1W_0 - Q0W_0)
+# Estimated MOR
+mean(Q1W_0)*(1-mean(Q0W_0))/((1-mean(Q1W_0))*mean(Q0W_0))
+# Step 2 estimation and prediction of the propensity score (ps)
+psm <- glm(A ~ W1 + W2 + W3 + W4, family = binomial, data = ObsData)
+gW = predict(psm, type = "response")
+summary(gW)
+# Step 3 computation of H (clever covariates) and estimation of epsilon
+H1W = ObsData$A / gW
+H0W = (1-ObsData$A) / (1 - gW)
+epsilon <- coef(glm(ObsData$Y ~ -1 + H0W + H1W + offset(qlogis(QAW_0)), family = binomial))
+# Step 4 Targeted estimate of the ATE and Marginal Odds Ratio
+Q1W_1 <- plogis(qlogis(Q1W_0) + epsilon[2] / gW)
+Q0W_1 <- plogis(qlogis(Q0W_0) + epsilon[1] / (1-gW))
+# ATE
+ATEtmle1 <- mean(Q1W_1 - Q0W_1); ATEtmle1
+cat("\n ATEtmle1_bias:", abs(True_ATE - ATEtmle1))
+cat("\n ATEtmle1_rel_bias:",abs(True_ATE - ATEtmle1)/True_ATE,"%")
+# Marginal OR
+tmle1.MOR <- mean(Q1W_1) * (1 - mean(Q0W_1)) / ((1 - mean(Q1W_1)) * mean(Q0W_1)); tmle1.MOR
+# Table to visualize the data
+psi <- Q1W_1 - Q0W_1
 
+library(DT)
+df <- round(cbind(Q1W_0, Q0W_0, gW, eps1=epsilon[1], eps2=epsilon[2], psi), digits = 4)
+renderDataTable({datatable(head(df, n = nrow(df)), options = list(pageLength = 5, digits = 3))})
+# Step 5 statistical inference (efficient influence curve)
+# Efficient influence curve ATE
+EY1tmle<-mean(Q1W_1)
+EY0tmle<-mean(Q0W_1)
+d1 <- ((ObsData$A) * (ObsData$Y - Q1W_1)/gW) + Q1W_1 - EY1tmle
+d0 <- ((1 - ObsData$A) * (ObsData$Y - Q0W_1))/(1 - gW) + Q0W_1 - EY0tmle
+IC <- d1 - d0
+n <- nrow(ObsData)
+varHat.IC <- var(IC) / n
+ATEtmle1CI <- c(ATEtmle1 - 1.96 * sqrt(varHat.IC), ATEtmle1 + 1.96 * sqrt(varHat.IC)); ATEtmle1;
+ATEtmle1CI
+
+
+# Efficient influence curve MOR
+ICmor_tmle <- (1 - EY0tmle) / EY0tmle / (1 - EY1tmle)^2 * d1 - EY1tmle / (1 - EY1tmle) / EY0tmle^2 *
+  d0
+varHat2.IC <- var(ICmor_tmle) / n
+tmle1_ORCI <- tmle1.MOR + c(-1.96,1.96)*sqrt(varHat2.IC); tmle1.MOR; tmle1_ORCI
+# Augmented inverse probability treatment weighting (AIPTW) estimator
+EY1aiptw <- mean((ObsData$A) * (ObsData$Y - Q1W_0) / gW + Q1W_0)
+EY0aiptw <- mean((1 - ObsData$A) * (ObsData$Y - Q0W_0) / (1 - gW) + Q0W_0)
+AIPTW_ATE <- EY1aiptw - EY0aiptw; AIPTW_ATE
+cat("\n AIPTW_bias:", abs(True_ATE - AIPTW_ATE))
+cat("\n AIPTW_rel_bias:",abs(True_ATE - AIPTW_ATE) / True_ATE,"%")
+D1 <- (ObsData$A) * (ObsData$Y - Q1W_0) / gW + Q1W_0 - EY1aiptw
+D0 <- (1 - ObsData$A) * (ObsData$Y - Q0W_0) / (1 - gW) + Q0W_0 - EY0aiptw
+varHat_AIPTW <- var(D1 - D0) / n
+# AIPTW ATE 95%CI
+ATEaiptw_CI <- c(AIPTW_ATE - 1.96 * sqrt(varHat_AIPTW), AIPTW_ATE + 1.96 *
+                   sqrt(varHat_AIPTW)); AIPTW_ATE; ATEaiptw_CI
+# AIPTW MOR 95%CI
+AIPTW_MOR <- (EY1aiptw * (1 - EY0aiptw))/((1 - EY1aiptw) * EY0aiptw);AIPTW_MOR
+ICmor_aiptw <- (1 - EY0aiptw) / EY0aiptw / (1 - EY1aiptw)^2 * D1 - EY1aiptw / (1 - EY1aiptw) /
+  EY0aiptw^2 * D0
+varHat_AIPTW2 <- var(ICmor_aiptw) / n
+MORaiptw_CI <-c(AIPTW_MOR - 1.96*sqrt(varHat_AIPTW2), AIPTW_MOR +
+                  1.96*sqrt(varHat_AIPTW2)); AIPTW_MOR; MORaiptw_CI
+# R-package tmle (base implementation includes SL.step, SL.glm and SL.glm.interaction)
+library(tmle)
+library(SuperLearner)
+TMLE2 <- tmle(Y = ObsData$Y, A = ObsData$A, W = ObsData[,c("W1", "W2", "W3", "W4")], family =
+                "binomial")
+#NOTE:
+#Note that the tmle function default bounds the probabilities in the clever covariate denominators at 0.025.
+#You can remove this bound by specifying gbound=0
+ATEtmle2 <- TMLE2$estimates$ATE$psi;ATEtmle2
+TMLE2$estimates$ATE$CI
+MORtmle2 <- TMLE2$estimates$OR$psi;MORtmle2
+TMLE2$estimates$OR$CI
+cat("\n ATEtmle2_bias:", abs(True_ATE - ATEtmle2))
+cat("\n ATEtmle2_Rel_bias:",abs(True_ATE - ATEtmle2) / True_ATE,"%")
+
+# R-package tmle with user-selected Super learner libraries
+library(tmle)
+library(SuperLearner)
+SL.library <- c("SL.glm","SL.step","SL.step.interaction", "SL.glm.interaction","SL.gam",
+                "SL.randomForest", "SL.rpart")
+TMLE3 <- tmle(Y = ObsData$Y,A = ObsData$A,W = ObsData [,c("W1", "W2", "W3", "W4")],
+              family = "binomial", Q.SL.library = SL.library,g.SL.library = SL.library)
+ATEtmle3 <- TMLE3$estimates$ATE$psi;ATEtmle3
+TMLE3$estimates$ATE$CI
+MORtmle3 <- TMLE3$estimates$OR$psi;MORtmle3
+TMLE3$estimates$OR$CI
+cat("\n ATEtmle3_bias:", abs(True_ATE - ATEtmle3))
+cat("\n ATEtmle3_rel_bias:",abs(True_ATE - ATEtmle3) / True_ATE,"%")
+
+
+
+
+#More complex data-generating process
+#Readers interested in simulating more complex dependence structures among the covariates W1-W4 could
+#potentially use the R-package simcausal (Sofrygin O, van der Laan MJ, Neugebauer R (2015). simcausal:
+#                                           Simulating Longitudinal Data with Causal Inference Applications. R package version 0.5).
+#See the example here below:
+devtools::install_github('osofr/simcausal', build_vignettes = FALSE, force = TRUE, dependencies = TRUE)
+
+library(simcausal)
+M <- DAG.empty()
+M <- M +
+  node("W1", # age (0/1); 1 -> high age
+       distr = "rbern",
+       prob = .5) +
+  node("W2", # ses (1/2/3/4/5); higher age, higher probability of belonging to upper class
+       distr = "rcat.b1",
+       probs = {
+         plogis(-3.1 + 0.05*W1); # upper middle class, 4%
+         plogis(-1.25 + 0.04*W1); # middle class, 22%
+         plogis(-0.05 + 0.03*W1); # lower middle 49%
+         plogis(-1.65 + 0.02*W1); # working class 16%
+         plogis(-2.3 + 0.01*W1)}) +
+  node("W3", #comorbidities (1/2/3/4/5);
+       distr = "rcat.b1",
+       probs = {
+         plogis(-0.8 + 0.005*W1 + 0.1*W2);
+         plogis(-0.1 + 0.010*W1 + 0.12*W2);
+         plogis(-1.2 + 0.015*W1 + 0.15*W2);
+         plogis(-1.6 + 0.020*W1 + 0.2*W2);
+         plogis(-2.5 + 0.025*W1 + 0.25*W2)}) +
+  node("W4", # stage (1/2/3/4); # the higher the worse
+       distr = "rcat.b1",
+       probs = {
+         plogis(-1 + 0.01*W1 - 0.04*W2);
+         plogis(-0.2 + 0.02*W1 - 0.05*W2);
+         plogis(-0.8 + 0.03*W1 - 0.055*W2);
+         plogis(-2 + 0.04*W1 - 0.1*W2)}) + 
+node("A", # a = 0 mono therapy; a = 1 dualtherapy
+     distr = "rbern",
+     prob = plogis(-1.4 + 0.05*W1 + 0.25*W3 + 0.1*exp(W4))) +
+node("Y", #y = 0 -> death; y = 1 -> alive
+       distr = "rbern",
+       prob = plogis(-3.4 + 0.75*A - 0.1*W1 + 0.35*W2 + 0.25*W3 + 0.20*sqrt(1/W4) - 0.9*A*W2 + 1.1*A*W3))
+Mset <- set.DAG(M)
+# simulate observed data
+Odat <- simcausal::sim(DAG = Mset, n = 10000, rndseed = 7693)
+# specify the two interventions
+a1 <- node("A", distr = "rbern", prob = 1)
+Mset <- Mset + action("a1", nodes = a1)
+a0 <- node("A", distr = "rbern", prob = 0)
+Mset <- Mset + action("a0", nodes = a0)
+# counterfactual data
+dat <- simcausal::sim(DAG = Mset, actions = c("a1", "a0"), n = 1000000, rndseed = 7693)
+head(dat[["a1"]]); head(dat[["a0"]])
+# E(y) under a=1 (chemo)
+Mset <- set.targetE(Mset, outcome = "Y", param = "a1")
+eval.target(Mset, data = dat)$res
+# E(y) unter a=0 (chemo and radio)
+Mset <- set.targetE(Mset, outcome = "Y", param = "a0")
+eval.target(Mset, data = dat)$res
+# ATE (additive scale)
+Mset <- set.targetE(Mset, outcome = "Y", param = "a1-a0")
+eval.target(Mset, data = dat)$res
+# multiplicative scale
+Mset <- set.targetE(Mset, outcome = "Y", param = "a1/a0")
+eval.target(Mset, data = dat)$res
+#DAG
+plotDAG(Mset, xjitter = 0.3, yjitter = 0.04,edge_attrs = list(width = 0.5, arrow.width = 0.2, arrow.size = 0.3),
+        vertex_attrs = list(size = 12, label.cex = 0.8))
 
 ### pay no attention beneath here!
 
@@ -131,11 +303,11 @@ exp(confint(naive))
 
 sl_libs <- c('SL.glmnet', 'SL.ranger', 'SL.nnet', 'SL.earth') # (penalized regression, random forests, and multivariate adaptive regression splines)
 
-Y <- dat_obs$Y
+Y <- ObsData$Y
 #W_A <- dat_obs %>% select(-Y) # remove the outcome to make a matrix of predictors (A, W1, W2, W3, W4) for SuperLearner
 
-A <- dat_obs$A
-W <- dat_obs %>% select(-Y, -A) # matrix of predictors that only contains the confounders W1, W2, W3, and W4
+A <- ObsData$A
+W <- ObsData %>% select(-Y, -A) # matrix of predictors that only contains the confounders W1, W2, W3, and W4
 
 tmle_fit <-
   tmle::tmle(Y = Y, # outcome vector
@@ -154,19 +326,6 @@ ATC <- c(tmle_fit$estimates$ATC$psi, tmle_fit$estimates$ATC$CI[1], tmle_fit$esti
 print(ATE)
 print(ATT)
 print(ATC)
-
-
-
-
-library(rcausal)
-dd <- tetradrunner(algoId = 'fges', df = dat, dataType = 'discrete', scoreId = 'bdeu', 
-                   #maxDegree=-1,
-                   faithfulnessAssumed=TRUE, verbose = TRUE)
-dd$graph
-graphDot <- tetradrunner.tetradGraphToDot(dd$graph)
-dot(graphDot)
-
-
 
 
 kable(head(dat_obs), digits=2, caption = "Simulated dataset.")
@@ -265,6 +424,33 @@ pretty_logistic_table <- function(model_fit) {
 
 glm(death ~ race, data = hospitalized_df, family = binomial()) %>%
   pretty_logistic_table()
+
+#######
+
+# Trying to get rcausal running
+
+
+library(rcausal)
+dd <- tetradrunner(algoId = 'gfci', df = dat, dataType = 'discrete', scoreId = 'bdeu', 
+                   #maxDegree=-1,
+                   faithfulnessAssumed=TRUE, verbose = TRUE)
+dd$graph
+graphDot <- tetradrunner.tetradGraphToDot(dd$graph)
+dot(graphDot)
+data("charity")    #Load the charity dataset
+tetradrunner <- tetradrunner(algoId = 'fges',df = charity,scoreId = 'sem-bic', dataType = 'continuous',faithfulnessAssumed=TRUE,maxDegree=-1,verbose=TRUE)    #Compute FGES search
+tetradrunner$nodes #Show the result's nodes
+tetradrunner$edges #Show the result's edges
+
+graph <- tetradrunner$graph
+graph$getAttribute('BIC')
+
+nodes <- graph$getNodes()
+for(i in 0:as.integer(nodes$size()-1)){
+  node <- nodes$get(i)
+  cat(node$getName(),": ",node$getAttribute('BIC'),"\n")
+}
+
 
 
 
