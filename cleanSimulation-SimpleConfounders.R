dbc7fabf4ba9d26ebeb21e59414d849be224158d
Israel O. Dilán-Pantojas <iod4@pitt.edu>
2022-10-11 17:12:01 -0400

    Update cleanSimulation-SimpleConfounders.R

--- cleanSimulation-SimpleConfounders.R
+++ cleanSimulation-SimpleConfounders.R
@@ -1,4 +1,3 @@
-
 # install all of these libraries if you haven't yet
 # first: install.packages("BiocManager")
 # then: 
@@ -24,33 +23,43 @@ library(WeightIt)
 set.seed(7) # so results are reproducible
 
 generate_data <- function(n, adjSetType) {
-  #n = 1000BiocManager::install(c("GenomicFeatures", "AnnotationDbi"))BiocManager::install(c("GenomicFeatures", "AnnotationDbi"))BiocManager::install(c("GenomicFeatures", "AnnotationDbi"))
-  W1 <- rbinom(n, size = 1, prob=0.2) # binary confounder
-  W2 <- rbinom(n, size = 1, prob=0.5) #binary confounder
+  
+  # Binary confounders
+  W1 <- rbinom(n, size = 1, prob=0.2)
+  W2 <- rbinom(n, size = 1, prob=0.5)
   W3 <- rbinom(n, size = 1, prob=0.3)
   W4 <- rbinom(n, size = 1, prob=0.2)
+  
+  # Binary treatment
   A <- rbinom(n, size=1, prob = plogis(-2+0.2*W1+0.1*W2+0.1*W3+0.1*W4)) # binary treatment
-  P1 <- rbinom(n, size=1, prob=0.4)
-  P2 <- rbinom(n, size=1, prob=0.3)
+  
+  # Precision variables
+  P1 <- rbinom(n, size=1, prob=0.4)  
+  P2 <- rbinom(n, size=1, prob=0.3) 
   P3 <- rbinom(n, size=1, prob=0.1)
-#  Y <- rbinom(n, size =1, prob=plogis(-1+A-0.1*W1+0.2*W2+0.1*W3+0.1*W4+0.1*P1+0.3*P2+0.1*P3))
-  # counterfactual
-  Y.1 <- rbinom(n, size=1, prob= plogis(-1 + 1 -0.1*w1 + 0.35*w2 + 0.25*w3 + 0.20*w4 + 0.15*w2*w4))
-  Y.0 <- rbinom(n, size=1, prob= plogis(-1 + 0 -0.1*w1 + 0.35*w2 + 0.25*w3 + 0.20*w4 + 0.15*w2*w4))
+  
+  # Counterfactuals variables
+  Y.1 <- rbinom(n, size=1, prob= plogis(-1 + 1 -0.1*W1 + 0.35*W2 + 0.25*W3 + 0.20*W4 + 0.15*W2*W4))
+  Y.0 <- rbinom(n, size=1, prob= plogis(-1 + 0 -0.1*W1 + 0.35*W2 + 0.25*W3 + 0.20*W4 + 0.15*W2*W4))
+  
   # Observed outcome
   Y <- Y.1*A + Y.0*(1 - A)
-   M1 <- rbinom(n, size=1, prob=plogis(1+0.4*A))
+   
+  M1 <- rbinom(n, size=1, prob=plogis(1+0.4*A))
   C1 <- rbinom(n, size=1, prob=plogis(-1+0.3*A+0.4*Y))
   M2 <- rbinom(n, size = 1, prob = plogis(-1+0.8*A))
   WM <- rbinom(n, size=1, prob=plogis(1+0.4*W3+0.7*M2))
   C2 <- rbinom(n, size=1, prob=plogis(-1+0.3*A+0.4*Y))
   M3 <- rbinom(n, size = 1, prob = plogis(-1+0.3*A))
   Chimera <- rbinom(n, size=1, prob=plogis(2+0.3*W4+0.1*M3+.4*C2))
+  
+  # Dataset composition
   if (adjSetType == "all") {
     dat <- data.frame(W1, W2, M1, C1, W3, M2, WM, C2, W4, M3, P1, P2, P3, Chimera, A, Y, Y.1, Y.0)
   } else if (adjSetType == "allPure") {
     dat <- data.frame(W1, W2, M1, C1, WM, P1, P2, P3, Chimera, A, Y, Y.1, Y.0)
-  } else if (adjSetType == "onlyConfounders") { dat <- data.frame(W1, W2, W3, W4, A, Y, Y.1, Y.0) 
+  } else if (adjSetType == "onlyConfounders") { 
+    dat <- data.frame(W1, W2, W3, W4, A, Y, Y.1, Y.0) 
   } else if (adjSetType == "onlyConfoundersAndPrecisionVars") {
     dat <- data.frame(W1, W2, P1, P2, P3, A, Y, Y.1, Y.0)
   } else if (adjSetType == "Confounders_C1_WM_Chimera") {
@@ -61,21 +70,27 @@ generate_data <- function(n, adjSetType) {
   return(dat)
 }
 
-generateData<- function(n){
+generateData <- function(n) {
+  
+  # Binary confounders
   w1 <- rbinom(n, size=1, prob=0.5)
   w2 <- rbinom(n, size=1, prob=0.65)
   w3 <- round(runif(n, min=0, max=4), digits=0)
   w4 <- round(runif(n, min=0, max=5), digits=0)
+  
+  # Binary treatment
   A <- rbinom(n, size=1, prob= plogis(-5 + 0.05*w2 + 0.25*w3 + 0.6*w4 + 0.4*w2*w4))
-  # the counterfactuals aka potential outcomes
+  
+  # Counterfactuals (Aka potential outcomes)
   Y.1 <- rbinom(n, size=1, prob= plogis(-1 + 1 -0.1*w1 + 0.35*w2 + 0.25*w3 + 0.20*w4 + 0.15*w2*w4))
   Y.0 <- rbinom(n, size=1, prob= plogis(-1 + 0 -0.1*w1 + 0.35*w2 + 0.25*w3 + 0.20*w4 + 0.15*w2*w4))
+  
   # Observed outcome
   Y <- Y.1*A + Y.0*(1 - A)
+  
   # return data.frame
-  data.frame(w1, w2, w3, w4, A, Y, Y.1, Y.0)
+  return(data.frame(w1, w2, w3, w4, A, Y, Y.1, Y.0))
 }
-#quantifyEffectWithSubset()
 
 n = 20000
 #dat_obs <- generate_data(n, "onlyBadCovariates")
@@ -85,54 +100,62 @@ dat_obs <- generate_data(n, "onlyConfounders")
 #dat_obs <- generate_data(n, "allPure")
 
 #dat <- data.frame(dat_obs)
-#
 #kable(head(dat_obs), digits=2, caption = "Simulated dataset.")
 
 # So we will start out with a simulation containing only
 # four "well-behaved" / "pure" confounders (W1, W2, W3, and W4)
-# plus the exposure (A) and outcome variables (Y)
+# plus the exposure (A) and outcome variables (Y).
+# We get this from the "onlyConfounders" alternative.
 #############
 
 # Now starting with where the original tutorial left off
-#  with a more solid data generating process
+# with a more solid data generating process
 
 # Source: https://onlinelibrary.wiley.com/action/downloadSupplement?doi=10.1002%2Fsim.7628&file=sim7628-sup-0003-Appendix.pdf
-#   also at: https://github.com/migariane/SIM-TMLE-tutorial/blob/master/Appendix_SIM_2018.pdf
+# Also at: https://github.com/migariane/SIM-TMLE-tutorial/blob/master/Appendix_SIM_2018.pdf
 
-# the gold standard: 
-
+# The gold standard:
 # True ATE in the population
 set.seed(7777)
 
 #dat_obs <- generate_data(n, "onlyBadCovariates")
 #dat_obs <- generate_data(n, "onlyConfounders")
 
+# Generate a dataset
 ObsDataTrueATE <- generate_data(n = 500000, "onlyConfounders")
+
+# Note the True Mean of the counter factuals
 True_EY.1 <- mean(ObsDataTrueATE$Y.1)
 True_EY.0 <- mean(ObsDataTrueATE$Y.0)
-True_ATE <- True_EY.1-True_EY.0 ;True_ATE
-# Below is the OR = ad/bc
-True_MOR <- (True_EY.1*(1-True_EY.0))/((1-True_EY.1)*True_EY.0);True_MOR
-cat("\n True_ATE:", abs(True_ATE))
-#
 
-# Data for analysis
+# Note the true average treatment effect (ATE) 
+True_ATE <- True_EY.1-True_EY.0; True_ATE
+cat("\n True_ATE:", abs(True_ATE))
+
+# Below is the Mean Odds Ratio (MOR)
+# OR = ad/bc
+# Calculated as the odds ratio of the mean counter factuals Y0 and Y1
+True_MOR <- (True_EY.1*(1-True_EY.0))/((1-True_EY.1)*True_EY.0); True_MOR
+cat("\n True_MOR:", abs(True_MOR))
+
+# Generate a Dataset for analysis
 set.seed(7722)
 ObsData <- generate_data(n = 20000, "onlyConfounders")
 write.csv(ObsData, "obsData.csv")
 
-
-# naive estimation - conditional odds ratio
-
+# Naive estimation - conditional odds ratio
 naive = glm(data = ObsData, Y ~ A + W1 + W2 + W3 + W4, family = binomial)
 summary(naive)
+
+# Should be relatively close to the MOR.
 exp(naive$coef[2])
-exp(confint(naive))
+print("!!! Not working !!!")
+# exp(confint(naive))
 # exponentiated odds ratio = 2.608
 # True mOR = 2.52 (!!!)
 
 ## TMLE implementation by hand
-# Step 1 estimation and prediction of the model for the outcome (G-computation)
+# Step 1: Estimation and prediction of the model for the outcome (G-computation)
 gm <- glm(data = ObsData, Y ~ A + W1 + W2 + W3 + W4, family = binomial)
 # Prediction for A, A = 1 and, A = 0
 QAW_0 <- predict(gm, type = "response")
